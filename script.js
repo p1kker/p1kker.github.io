12af0f6245669e795506659138051967ccb485dd
Elijah W <nalahi8598@gmail.com>
2024-08-02 00:28:47 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
